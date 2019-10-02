c5bd1b59cb54110d8e7ad78837df4a1869c0ce2e
Lucian <lucian.covic@gmail.com>
2019-10-01 20:43:53 -0400

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Navbar />
+          <h1>Workout App</h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
