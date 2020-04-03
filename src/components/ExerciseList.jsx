60c98009d4eb28b3dcf2b8bbd1109ed1b14f71af
Lucian <lucian.covic@gmail.com>
2020-04-03 17:49:52 -0400

    added exerciseList css card flipping animation

--- src/components/ExerciseList.jsx
+++ src/components/ExerciseList.jsx
@@ -21,45 +21,92 @@ function ExerciseList(props) {
 
 
     return (
-      <div className="exerciseList" onClick={() => {
-        selectExercise({
-          exerciseTitle: exerciseTitle,
-          steps: steps
-        })
-      }}>
-        <div className="exerciseTitle">
-          <h1 className="exercise-title">{exerciseTitle}</h1>
-        </div>
-        <div className="exerciseImageAndInfo">
-          <ExerciseImage
-            exerciseImage1={exerciseImage1}
-            exerciseImage2={exerciseImage2}
-          />
-          <div className="exercise-info">
-            <h1 className="general-info">{exercisePrimer}</h1>
-            <div className="mini-info-container">
-              <h2 className="mini-info">
-                Movement type:{" "}
-                {exerciseType
-                  ? exerciseType.charAt(0).toUpperCase() +
-                    exerciseType.substring(1)
-                  : null}
-              </h2>
-              <br />
-              <h2 className="mini-info">
-                Equipment:{" "}
-                {exerciseEquipment
-                  ? exerciseEquipment
-                      .join(", ")
-                      .charAt(0)
-                      .toUpperCase() + exerciseEquipment.join(", ").substring(1)
-                  : null}
-              </h2>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <div
+              className="exerciseList"
+              onClick={() => {
+                selectExercise({
+                  exerciseTitle: exerciseTitle,
+                  steps: steps
+                });
+              }}
+            >
+              <div className="exerciseTitle">
+                <h1 className="exercise-title">{exerciseTitle}</h1>
+              </div>
+              <div className="exerciseImageAndInfo">
+                <ExerciseImage
+                  exerciseImage1={exerciseImage1}
+                  exerciseImage2={exerciseImage2}
+                />
+                <div className="exercise-info">
+                  <h1 className="general-info">{exercisePrimer}</h1>
+                  <div className="mini-info-container">
+                    <h2 className="mini-info">
+                      Movement type:{" "}
+                      {exerciseType
+                        ? exerciseType.charAt(0).toUpperCase() +
+                          exerciseType.substring(1)
+                        : null}
+                    </h2>
+                    <br />
+                    <h2 className="mini-info">
+                      Equipment:{" "}
+                      {exerciseEquipment
+                        ? exerciseEquipment
+                            .join(", ")
+                            .charAt(0)
+                            .toUpperCase() +
+                          exerciseEquipment.join(", ").substring(1)
+                        : null}
+                    </h2>
+                  </div>
+                  <p className="more-info">More info</p>
+                </div>
+              </div>
             </div>
-            <p className="more-info">More info</p>
+          </div>
+          {/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+          <div className="flip-card-back">
+            <div className="exerciseList">
+              <div className="exerciseTitle">
+                <h1 className="exercise-title">{exerciseTitle}</h1>
+              </div>
+              <div className="exerciseImageAndInfo">
+                <ExerciseImage
+                  exerciseImage1={exerciseImage1}
+                  exerciseImage2={exerciseImage2}
+                />
+                <div className="exercise-info">
+                  <h1 className="general-info">{exercisePrimer}</h1>
+                  <div className="mini-info-container">
+                    <h2 className="mini-info">
+                      Movement type:{" "}
+                      {exerciseType
+                        ? exerciseType.charAt(0).toUpperCase() +
+                          exerciseType.substring(1)
+                        : null}
+                    </h2>
+                    <br />
+                    <h2 className="mini-info">
+                      Equipment:{" "}
+                      {exerciseEquipment
+                        ? exerciseEquipment
+                            .join(", ")
+                            .charAt(0)
+                            .toUpperCase() +
+                          exerciseEquipment.join(", ").substring(1)
+                        : null}
+                    </h2>
+                  </div>
+                  <p className="more-info">More info</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        
       </div>
     );
 
--- src/components/ExerciseModalContent.jsx
+++ src/components/ExerciseModalContent.jsx
@@ -9,10 +9,22 @@ export default function ExerciseModalContent(props) {
 
   return (
     <div>
-      <h1>{data.exerciseTitle}</h1>
-      <p>{data.steps}</p>
-
-      <button onClick={props.closeModal}>Close</button>
+      <h1 className="modal-title">{data.exerciseTitle}</h1>
+      <br></br>
+      <ol className="steps-list">
+        {data.steps.map(step => {
+          return (
+            <li className="step-list-item">
+              {step}
+            </li>
+          );
+        })}
+      </ol>
+      <div className="modal-button-div">
+        <button className="modal-button" onClick={props.closeModal}>
+          Close
+        </button>
+      </div>
     </div>
   );
 }
