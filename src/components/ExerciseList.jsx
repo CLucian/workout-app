098a968f848764a9c9bb0662bda9488efc87e3f3
Lucian <lucian.covic@gmail.com>
2020-04-10 19:04:01 -0400

    Added more colors and styles to the exerciseList tab and to Overview tab

--- src/components/ExerciseList.jsx
+++ src/components/ExerciseList.jsx
@@ -22,150 +22,153 @@ function ExerciseList(props) {
 
     return (
       <>
-<div class="flip">
-      <div class="front">
-        <div
-          className="exerciseList"
-          onClick={() => {
-            selectExercise({
-              exerciseTitle: exerciseTitle,
-              steps: steps
-            });
-          }}
-        >
-      <div className="exerciseTitle">
-        <h1 className="exercise-title">{exerciseTitle}</h1>
-      </div>
-      <div className="exerciseImageAndInfo">
-        <ExerciseImage
-          exerciseImage1={exerciseImage1}
-          exerciseImage2={exerciseImage2}
-        />
-        <div className="exercise-info">
-          <h1 className="general-info">{exercisePrimer}</h1>
-          <div className="mini-info-container">
-            <h2 className="mini-info">
-              Movement type:{" "}
-              {exerciseType
-                ? exerciseType.charAt(0).toUpperCase() +
-                  exerciseType.substring(1)
-                : null}
-            </h2>
-            <br />
-            <h2 className="mini-info">
-              Equipment:{" "}
-              {exerciseEquipment
-                ? exerciseEquipment
-                    .join(", ")
-                    .charAt(0)
-                    .toUpperCase() +
-                  exerciseEquipment.join(", ").substring(1)
-                : null}
-            </h2>
+        <div class="flip">
+          <div class="front">
+            <div
+              className="exerciseList"
+              onClick={() => {
+                selectExercise({
+                  exerciseTitle: exerciseTitle,
+                  steps: steps
+                });
+              }}
+            >
+              <div className="exerciseTitle">
+                <h1 className="exercise-title">{exerciseTitle}</h1>
+              </div>
+              <div className="exerciseImageAndInfo">
+                <ExerciseImage
+                  exerciseImage1={exerciseImage1}
+                  exerciseImage2={exerciseImage2}
+                />
+                <div className="exercise-info">
+                  <h1 className="general-info">{exercisePrimer}</h1>
+                  <div className="mini-info-container">
+                    <h2 className="mini-info-title">
+                      Movement type:{" "}
+                      <h2 className="mini-info">
+                      {exerciseType
+                        ? exerciseType.charAt(0).toUpperCase() +
+                          exerciseType.substring(1)
+                        : null}
+                        </h2>
+                    </h2>
+                    <br />
+                    <h2 className="mini-info-title">
+                      Equipment:{" "}
+                      <h2 className="mini-info">
+                        {exerciseEquipment
+                          ? exerciseEquipment
+                              .join(", ")
+                              .charAt(0)
+                              .toUpperCase() +
+                            exerciseEquipment.join(", ").substring(1)
+                          : null}
+                      </h2>
+                    </h2>
+                  </div>
+                  {/* <p className="more-info">More info</p> */}
+                </div>
+              </div>
+            </div>
           </div>
-          {/* <p className="more-info">More info</p> */}
-        </div>
-      </div>
-    </div>
-  </div>
 
-      <div class="back">
-        <div className="exerciseList">
-          <div className="exerciseTitle">
-            <h1 className="exercise-title">{exerciseTitle}</h1>
-          </div>
-          <div className="exerciseImageAndInfo">
-            <ExerciseImage
-              exerciseImage1={exerciseImage1}
-              exerciseImage2={exerciseImage2}
-            />
-            <div className="exercise-info">
-              <h1 className="general-info">Steps</h1>
-                <ol className="steps-list">
-                  {steps.map(step => {
-                    return <li className="step-list-item">{step}</li>;
-                  })}
-                </ol>
+          <div class="back">
+            <div className="exerciseList">
+              <div className="exerciseTitle">
+                <h1 className="exercise-title">{exerciseTitle}</h1>
+              </div>
+              <div className="exerciseImageAndInfo">
+                <ExerciseImage
+                  exerciseImage1={exerciseImage1}
+                  exerciseImage2={exerciseImage2}
+                />
+                <div className="exercise-info">
+                  <h1 className="steps-list-title">Steps</h1>
+                  <ol className="steps-list">
+                    {steps.map(step => {
+                      return <li className="step-list-item">{step}</li>;
+                    })}
+                  </ol>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-</div>
-</>
-// {/* 
+      </>
+      // {/*
 
-
-//       // <div className="flip-card">
-//       //   <div className="flip-card-inner">
-//       //     <div className="flip-card-front">
-//       //       <div
-//       //         className="exerciseList"
-//       //         onClick={() => {
-//       //           selectExercise({
-//       //             exerciseTitle: exerciseTitle,
-//       //             steps: steps
-//       //           });
-//       //         }}
-//       //       >
-//       //         <div className="exerciseTitle">
-//       //           <h1 className="exercise-title">{exerciseTitle}</h1>
-//       //         </div>
-//       //         <div className="exerciseImageAndInfo">
-//       //           <ExerciseImage
-//       //             exerciseImage1={exerciseImage1}
-//       //             exerciseImage2={exerciseImage2}
-//       //           />
-//       //           <div className="exercise-info">
-//       //             <h1 className="general-info">{exercisePrimer}</h1>
-//       //             <div className="mini-info-container">
-//       //               <h2 className="mini-info">
-//       //                 Movement type:{" "}
-//       //                 {exerciseType
-//       //                   ? exerciseType.charAt(0).toUpperCase() +
-//       //                     exerciseType.substring(1)
-//       //                   : null}
-//       //               </h2>
-//       //               <br />
-//       //               <h2 className="mini-info">
-//       //                 Equipment:{" "}
-//       //                 {exerciseEquipment
-//       //                   ? exerciseEquipment
-//       //                       .join(", ")
-//       //                       .charAt(0)
-//       //                       .toUpperCase() +
-//       //                     exerciseEquipment.join(", ").substring(1)
-//       //                   : null}
-//       //               </h2>
-//       //             </div>
-//       //             {/* <p className="more-info">More info</p> */}
-//       //           </div>
-//       //         </div>
-//       //       </div>
-//       //     </div>
-//       //     {/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
-//       //     <div className="flip-card-back">
-//       //       <div className="exerciseList">
-//       //         <div className="exerciseTitle">
-//       //           <h1 className="exercise-title">{exerciseTitle}</h1>
-//       //         </div>
-//       //         <div className="exerciseImageAndInfo">
-//       //           <ExerciseImage
-//       //             exerciseImage1={exerciseImage1}
-//       //             exerciseImage2={exerciseImage2}
-//       //           />
-//       //           <div className="exercise-info">
-//       //             <h1 className="general-info">Steps</h1>
-//       //               <ol className="steps-list">
-//       //                 {steps.map(step => {
-//       //                   return <li className="step-list-item">{step}</li>;
-//       //                 })}
-//       //               </ol>
-//       //           </div>
-//       //         </div>
-//       //       </div>
-//       //     </div>
-//       //   </div>
-//       // </div> */}
+      //       // <div className="flip-card">
+      //       //   <div className="flip-card-inner">
+      //       //     <div className="flip-card-front">
+      //       //       <div
+      //       //         className="exerciseList"
+      //       //         onClick={() => {
+      //       //           selectExercise({
+      //       //             exerciseTitle: exerciseTitle,
+      //       //             steps: steps
+      //       //           });
+      //       //         }}
+      //       //       >
+      //       //         <div className="exerciseTitle">
+      //       //           <h1 className="exercise-title">{exerciseTitle}</h1>
+      //       //         </div>
+      //       //         <div className="exerciseImageAndInfo">
+      //       //           <ExerciseImage
+      //       //             exerciseImage1={exerciseImage1}
+      //       //             exerciseImage2={exerciseImage2}
+      //       //           />
+      //       //           <div className="exercise-info">
+      //       //             <h1 className="general-info">{exercisePrimer}</h1>
+      //       //             <div className="mini-info-container">
+      //       //               <h2 className="mini-info">
+      //       //                 Movement type:{" "}
+      //       //                 {exerciseType
+      //       //                   ? exerciseType.charAt(0).toUpperCase() +
+      //       //                     exerciseType.substring(1)
+      //       //                   : null}
+      //       //               </h2>
+      //       //               <br />
+      //       //               <h2 className="mini-info">
+      //       //                 Equipment:{" "}
+      //       //                 {exerciseEquipment
+      //       //                   ? exerciseEquipment
+      //       //                       .join(", ")
+      //       //                       .charAt(0)
+      //       //                       .toUpperCase() +
+      //       //                     exerciseEquipment.join(", ").substring(1)
+      //       //                   : null}
+      //       //               </h2>
+      //       //             </div>
+      //       //             {/* <p className="more-info">More info</p> */}
+      //       //           </div>
+      //       //         </div>
+      //       //       </div>
+      //       //     </div>
+      //       //     {/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+      //       //     <div className="flip-card-back">
+      //       //       <div className="exerciseList">
+      //       //         <div className="exerciseTitle">
+      //       //           <h1 className="exercise-title">{exerciseTitle}</h1>
+      //       //         </div>
+      //       //         <div className="exerciseImageAndInfo">
+      //       //           <ExerciseImage
+      //       //             exerciseImage1={exerciseImage1}
+      //       //             exerciseImage2={exerciseImage2}
+      //       //           />
+      //       //           <div className="exercise-info">
+      //       //             <h1 className="general-info">Steps</h1>
+      //       //               <ol className="steps-list">
+      //       //                 {steps.map(step => {
+      //       //                   return <li className="step-list-item">{step}</li>;
+      //       //                 })}
+      //       //               </ol>
+      //       //           </div>
+      //       //         </div>
+      //       //       </div>
+      //       //     </div>
+      //       //   </div>
+      //       // </div> */}
     );
 
 }
--- src/components/OverviewTab.jsx
+++ src/components/OverviewTab.jsx
@@ -4,27 +4,22 @@ function OverviewTab(props) {
 
 	const { title, info, muscleFunction, src } = props;
 
-	return(
-		<div className="main-container">
-			<div className="category-title">{title}</div>
-			<div className="img-info-container">
-				<div className="img-container">
-					<img
-						className="overview-img"
-						src={src}
-						alt="abs"
-					/>
-				</div>
-				<div className="info-container">
-					<div className="info">
-						{info}
-					</div>
-					<h1 className="muscleFunction-title">Muscle Function</h1>
-					<div className="muscleFunction">{muscleFunction}</div>
-				</div>
-			</div>
-		</div>
-	)
+	return (
+    <div className="main-container">
+      <div className="category-title">{title}</div>
+      <div className="img-info-container">
+        <div className="img-container">
+          <img className="overview-img" src={src} alt="abs" />
+        </div>
+        <div className="info-container">
+          <h1 className="muscleFunction-title">General Info</h1>
+          <div className="info">{info}</div>
+          <h1 className="muscleFunction-title">Muscle Function</h1>
+          <div className="muscleFunction">{muscleFunction}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default OverviewTab;
